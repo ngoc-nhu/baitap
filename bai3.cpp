c55278601f07949f7083b0729443ae76ce5eeb22
ngoc-nhu <80908216+ngoc-nhu@users.noreply.github.com>
2021-03-22 16:42:39 +0700

    Add files via upload

---
+++ bai3.cpp
@@ -0,0 +1,26 @@
+﻿#include<iostream>
+using namespace std;
+void xl_hoc_luc(int dtb) {
+	if (dtb >= 9 && dtb <= 10) {
+		cout << " GIIOI " << endl;
+	}
+	else if (dtb >= 7 && dtb <= 8) {
+		cout << " KHA " << endl;
+	}
+	else if (dtb >= 4 && dtb <= 6) {
+		cout << " TRUNG BINH " << endl;
+	}
+	else {
+		cout << " YEU " << endl;
+	}
+}
+int main() {
+	int dtb;
+	do {
+		cout << " nhap diem trung binh = ";
+		cin >> dtb;
+
+	} while (dtb > 10 || dtb < 0);
+	cout << " xep loai hoc luc la ";
+	xl_hoc_luc(dtb);
+}
