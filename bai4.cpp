710c9ac83c69ea047c3402b8837296906456c046
ngoc-nhu <80908216+ngoc-nhu@users.noreply.github.com>
2021-03-22 16:29:13 +0700

    Add files via upload

---
+++ bai4.cpp
@@ -0,0 +1,17 @@
+﻿#include<iostream>
+using namespace std;
+void hoan_vi(int& a, int& b) {
+	int tam = a;
+	a = b;
+	b = tam;
+}
+int main() {
+	int a, b;
+	cout << " nhap a = ";
+	cin >> a;
+	cout << " nhap b = ";
+	cin >> b;
+	hoan_vi(a, b);
+	cout << " sau khi hoan vi ";
+	cout << a << " " << b;
+}
