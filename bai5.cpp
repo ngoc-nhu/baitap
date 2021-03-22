96069ec66a12c57e2ecf45629af0351443637f42
ngoc-nhu <80908216+ngoc-nhu@users.noreply.github.com>
2021-03-22 16:45:28 +0700

    Add files via upload

---
+++ bai5.cpp
@@ -0,0 +1,13 @@
+﻿#include<iostream>
+using namespace std;
+int tinh_nam_sinh(int n) {
+	return 2021 - n;
+}
+int main() {
+	int n;
+	cout << " nhap tuoi ";
+	cin >> n;
+	int namsinh = tinh_nam_sinh(n);
+	cout << " nam sinh la ";
+	cout << namsinh;
+}
