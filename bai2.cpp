63fe754da5900e234479b62b17d37d10c26f8a57
ngoc-nhu <80908216+ngoc-nhu@users.noreply.github.com>
2021-03-22 17:02:06 +0700

    Add files via upload

---
+++ bai2.cpp
@@ -0,0 +1,30 @@
+﻿#include<iostream>
+using namespace std;
+void tim_max(int n) {
+	int min, max, a, i;
+	max = 1;
+	min = 1000000;
+	for (i = 1; i <= n; i++) {
+		cout << " nhap phan thu  " << i << " = ";
+		cin >> a;
+		if (max < a) {
+			max = a;
+		}
+		if (min > a) {
+			min = a;
+		}
+
+	}
+	cout << " so lon nhat la ";
+	cout << max << endl;
+	cout << " so nho nhat la ";
+	cout << min;
+
+}
+int main() {
+	int n;
+	cout << " nhap n = ";
+	cin >> n;
+	tim_max(n);
+
+}
